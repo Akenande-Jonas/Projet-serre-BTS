5485422d343e984c281dcf85ac637f633d191bb1
Jonas <jonasakenande7@gmail.com>
2026-03-03 09:12:18 +0100

    update de inscription.js

--- front/inscription.js
+++ front/inscription.js
@@ -1,4 +1,4 @@
-const API_URL = "http://172.29.16.154/api";
+const API_URL = "http://172.29.60.60/api";
 
 async function register() {
     const prenom = document.getElementById("prenom").value.trim();
